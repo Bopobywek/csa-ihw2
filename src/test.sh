f197076d2de0581154d91ca9a2ac1f9d3babbe69
Andrey Nechesov <alnechesov@edu.hse.ru>
2022-11-13 23:10:58 +0300

    Report completed

--- src/test.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#gcc optimized.s -fsanitize=address -o optimized
-gcc optimized.s -o optimized
-../checker.py ./optimized ../tests/
-../checker.py -f ./optimized ../tests/
-./optimized -t 10
-./optimized -r 3
-
