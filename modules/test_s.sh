8c728791f49581f8965573e483fd0a2cdf3b5af1
Andrey Nechesov <alnechesov@edu.hse.ru>
2022-11-13 23:13:12 +0300

    Report completed

--- modules/test_s.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-#gcc optimized.s -fsanitize=address -o optimized
-#gcc optimized.s -o optimized
-../checker.py ./mod ../tests/
-../checker.py -f ./mod ../tests/
-./mod -t 10
-./mod -r 3
-
